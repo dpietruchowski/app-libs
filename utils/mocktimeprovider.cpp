2fd6331e4a6fad1de6a7e51963143e294206b9d4
dpietruchowski <dpietruchowski+dev@gmail.com>
2026-01-05 16:43:17 +0100

    Add advanceDays and advanceDate methods to MockTimeProvider

--- utils/mocktimeprovider.cpp
+++ utils/mocktimeprovider.cpp
@@ -7,6 +7,29 @@ void MockTimeProvider::setCurrentDateTime(const QDateTime& dateTime)
 
 void MockTimeProvider::setCurrentDate(const QDate& date) { m_currentDate = date; }
 
+void MockTimeProvider::advanceDays(int days)
+{
+    QDate current = m_currentDate.isValid() ? m_currentDate : QDate::currentDate();
+    m_currentDate = current.addDays(days);
+    
+    if (m_currentDateTime.isValid())
+    {
+        m_currentDateTime = m_currentDateTime.addDays(days);
+    }
+}
+
+void MockTimeProvider::advanceDate(const QDate& targetDate)
+{
+    m_currentDate = targetDate;
+    
+    if (m_currentDateTime.isValid())
+    {
+        QDate current = m_currentDateTime.date();
+        int days = current.daysTo(targetDate);
+        m_currentDateTime = m_currentDateTime.addDays(days);
+    }
+}
+
 QDateTime MockTimeProvider::currentDateTime() const
 {
     return m_currentDateTime.isValid() ? m_currentDateTime : QDateTime::currentDateTime();
--- utils/mocktimeprovider.h
+++ utils/mocktimeprovider.h
@@ -10,6 +10,9 @@ public:
     void setCurrentDateTime(const QDateTime& dateTime);
     void setCurrentDate(const QDate& date);
 
+    void advanceDays(int days);
+    void advanceDate(const QDate& targetDate);
+
     QDateTime currentDateTime() const override;
     QDate currentDate() const override;
 
