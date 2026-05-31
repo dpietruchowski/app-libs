aaf82d6e19090a0af278c7a7e830ec2d538e65c4
dpietruchowski <dpietruchowski+dev@gmail.com>
2026-05-31 22:28:06 +0200

    Show answer feedback in FillInContent when context is disabled

--- utils/fillincontent.cpp
+++ utils/fillincontent.cpp
@@ -18,25 +18,32 @@ QString FillInContent::content() const
 
 QString FillInContent::buildSentence() const
 {
-    if (!m_showSentence)
-        return m_preview;
-    if (m_entry.sentence.isEmpty())
-        return {};
+    const bool noContext = !m_showSentence;
+    const QString target = noContext ? m_entry.expression : m_entry.blankedText;
+    const QString text = noContext ? m_entry.expression : m_entry.sentence;
 
-    FillInTextBuilder builder(QStringList { m_entry.blankedText }, m_entry.sentence);
+    if (noContext && !m_interactive)
+        return m_preview;
+    if (text.isEmpty())
+        return noContext ? m_preview : QString {};
+
+    FillInTextBuilder builder(QStringList { target }, text);
     if (!m_preview.isEmpty())
         return builder.buildPreviewText(m_preview);
     if (!m_interactive)
         return builder.buildHighlightedText();
+    if (m_userAnswer.isEmpty())
+        return noContext ? m_preview : builder.buildPartialText();
 
-    if (!m_userAnswer.isEmpty())
-        builder.setAttempt(0, m_userAnswer);
+    builder.setAttempt(0, m_userAnswer);
     return builder.buildPartialText();
 }
 
 QString FillInContent::buildHeader() const
 {
     if (!m_showExpression || m_entry.expression.isEmpty())
+        return {};
+    if (!m_showSentence && m_interactive)
         return {};
     return "<p><b>" + m_entry.expression + "</b></p>";
 }
