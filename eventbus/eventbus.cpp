9cbf4e6d93553842d5d01dce6c226a121071f6ed
dpietruchowski <dpietruchowski+dev@gmail.com>
2026-06-12 19:59:03 +0200

    Fix EventBus: avoid -Wpotentially-evaluated-expression on Android

--- eventbus/eventbus.cpp
+++ eventbus/eventbus.cpp
@@ -7,7 +7,8 @@ void EventBus::publish(std::shared_ptr<EventBase> event)
         return;
     }
 
-    auto it = m_handlers.find(std::type_index(typeid(*event)));
+    const EventBase& eventRef = *event;
+    auto it = m_handlers.find(std::type_index(typeid(eventRef)));
     if (it != m_handlers.end())
     {
         for (const auto& handler : it->second)
